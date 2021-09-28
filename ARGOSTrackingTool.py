15dd382eed75658895a6aa3d6c54d471008c23b1
shanashapiro <89433900+shanashapiro@users.noreply.github.com>
2021-09-28 13:08:54 -0400

    Displays location for user date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,6 +9,10 @@
 # Date:   Fall 2021
 #--------------------------------------------------------------
 
+#Ask user for a start date
+user_date = input("Enter data to search for Sara: ")
+
+
 #Create a variable pointing to the data file 
 file_name = './data/raw/sara.txt'
 
@@ -40,14 +44,30 @@ for lineString in line_list:
     obs_lon = lineData[7]
     
     #Print the location of Sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},long:{obs_lon} on {obs_date}")
+   # print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},long:{obs_lon} on {obs_date}")
     
    # if =1,2,3, 
        # date_dict[record_id] = obs_date #looks like a number but being pulled as a string first. Nominal value (unique code, better kept as a string)
        # coord_dict[record_id] = (obs_lat,obs_lon)
        
     if obs_lc in ("1","2","3"):
-        print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+       # print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
         date_dict[record_id] = obs_date
         coord_dict[record_id] = (obs_lat,obs_lon)
+        
+#Create empty list to hold matching keys 
+matching_keys = []
 
+#Loop through items in the date_dict, and collect keys for matching dates
+for date_item in date_dict.items(): 
+    #get the key and  date of the item 
+    the_key, the_date = date_item
+    #see if the date matches the user date 
+    if the_date == user_date: 
+        matching_keys.append(the_key)
+        
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys: 
+    the_coords = coord_dict[matching_key]
+    obs_date = date_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},long:{obs_lon} on {obs_date}")
